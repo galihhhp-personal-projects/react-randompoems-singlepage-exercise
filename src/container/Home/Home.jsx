d60fdac76413239710d9f607d8ad8f4a470b7432
galihhhp <pangestu.galihhhp@gmail.com>
2020-12-25 04:59:43 +0700

    add hero section

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import NavbarComponent from './../Navbar-one/Navbar'
+// import MainComponent from '../MainComponent/Main'
+import HeroComponent from '../HeroComponent/Hero'
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <NavbarComponent />
+                <HeroComponent />
+
+                {/* <MainComponent /> */}
             </div>
         );
     }
 }
 
 export default Home;
+
+
+// {this.state.post.map((post) => {
+//     return (
+//       <Post
+//         key={post.id}
+//         data={post}
+//         remove={this.handleRemove}
+//         update={this.handleUpdate}
+//       />
+//     );
+
+
+
+
